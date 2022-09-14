9a50417cd74e97a3aa7ff4d68da4e941ebd61b3a
Akiranya <porridgen@gmail.com>
2022-09-15 01:01:08 +0800

    Interface is not implementation

---
+++ src/main/java/com/ryandw11/structure/bottomfill/BottomFill.java
@@ -0,0 +1,26 @@
+package com.ryandw11.structure.bottomfill;
+
+import com.ryandw11.structure.structure.Structure;
+import org.bukkit.Location;
+
+import java.io.IOException;
+
+/**
+ * The interface for bottom fill implementations.
+ *
+ * <p>Use the {@link BottomFillProvider} to get the correct implementation and
+ * register a new one.</p>
+ */
+public interface BottomFill {
+    /**
+     * Called by the plugin when a bottom fill should be performed.
+     *
+     * <p>This will only be called if the BottomFill option is enabled.</p>
+     *
+     * @param structure     The structure that was spawned.
+     * @param spawnLocation The spawn (paste) location of the structure.
+     * @param minLoc        The minimum location of the structure schem.
+     * @param maxLoc        The maximum location of the structure schem.
+     */
+    void performFill(Structure structure, Location spawnLocation, Location minLoc, Location maxLoc) throws IOException;
+}
