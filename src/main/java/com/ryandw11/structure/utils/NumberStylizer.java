eeb7587273e917a7d18d5ef757c6140e54aa5be4
Akiranya <porridgen@gmail.com>
2022-10-20 20:31:32 +0800

    Improve javadoc & code cleanup

--- src/main/java/com/ryandw11/structure/utils/NumberStylizer.java
+++ src/main/java/com/ryandw11/structure/utils/NumberStylizer.java
@@ -3,26 +3,24 @@ package com.ryandw11.structure.utils;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * This is the system that allows for a random range or exact numbers to be inputted in the config.
- *
- * Valid Inputs:
- *
- * [4;10] --> Picks a random number between 4 and 10.
- * 10     --> The number 10.
+ * This is the system that allows for a random range or exact numbers to be inputted in the config. Valid Inputs:
+ * <pre>
+ * {@code [4;10]} --> Picks a random number between 4 and 10.
+ * {@code 10}     --> The number 10.
+ * </pre>
  */
 public final class NumberStylizer {
 
     /**
-     * Allows for the input of a random range or an exact number.
-     * <p>Valid Inputs:</p>
+     * Allows for the input of a random range or an exact number. Valid Inputs:
+     * <p>
+     * <pre>
+     * {@code [4;10]} --> Picks a random number between 4 and 10.
+     * {@code 10}     --> The number 10.
+     * </pre>
      *
-     * <code>
-     *     [4;10]<br>
-     *     10
-     * </code>
-     *
-     * @param input The String input.
-     * @return The number. (If invalid 1 is returned).
+     * @param input the String input
+     * @return the number (if invalid 1 is returned)
      */
     public static int getStylizedInt(String input) {
         // [20;25]
@@ -32,13 +30,10 @@ public final class NumberStylizer {
             try {
                 int num1 = Integer.parseInt(out[0]);
                 int num2 = Integer.parseInt(out[1]);
-
                 return ThreadLocalRandom.current().nextInt(num1, num2 + 1);
-
             } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
                 return 1;
             }
-
         }
         // if just num
         else {
@@ -49,4 +44,5 @@ public final class NumberStylizer {
             }
         }
     }
+
 }
